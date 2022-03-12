dc5f7e03f606a516bbfa4dbcd784691c6d32e7e6
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-03-12 15:25:42 -0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <div id="menu">
+      <div id="brand">
+        <router-link to="/">
+          <img src="/images/logo.png">
+        </router-link>
+      </div>
+      <div id="side">
+        <router-link to="/browse">
+          <div class="athletes browse">
+            <img src="/images/browse.png">
+            <p>Browse</p>
+          </div> 
+        </router-link>
+        <router-link to="/compare">
+          <div class="menu-item">
+            <img src="/images/love.png">
+            <p> {{iNum}} Items</p>
+          </div>
+        </router-link>
+      </div>
+    </div>
+    <router-view/>
+    <div class="footer-container">
+      <div class="footer">
+        <a href="https://github.com/bunnycustard/cp3"><img src="/images/github.png"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+    }
+  },
+  computed: {
+    iNum() {
+      return this.$root.$data.compare.length;
+    }
+  },
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 50px 100px;
+}
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 5px;
+  grid-template-areas: "none brand side";
+  margin-bottom: 50px;
+}
+#menu a {
+  color: #000000;
+}
+#brand {
+  grid-area: brand;
+  display: flex;
+  justify-content: center;
+}
+#brand img {
+  height: 200px;
+}
+#side {
+  grid-area: side;
+  display: flex;
+  justify-content: flex-end;
+}
+#side img {
+  width: 50px;
+}
+.menu-item {
+  display: flex;
+  flex-direction: column;
+}
+.menu-item p {
+  margin: 0px;
+}
+.browse {
+  margin-right: 50px;
+}
+</style>
