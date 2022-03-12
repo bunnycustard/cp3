5e47736ef3b00e211c7d79ccd98346c242f848f3
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-03-12 16:40:47 -0700

    Create Compare.vue

---
+++ src/views/Compare.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+    <div>
+        <CompareList :compare="compare" />
+    </div>
+</template>
+
+<script>
+import CompareList from "../components/CompareList.vue"
+export default {
+  name: 'CompareComp',
+  components: {
+    CompareList
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    compare() {
+      return this.$root.$data.compare;
+    }
+  },
+}
+</script>
