1cf701e32f4533f060e0f0f87fc27e3bd9bc184d
bunnycustard <94877488+bunnycustard@users.noreply.github.com>
2022-03-12 15:52:22 -0700

    Create mock-data.js

---
+++ src/mock-data.js
@@ -0,0 +1,145 @@
+let mock = [{
+    id: 1,
+    name: "Aljamain Sterling",
+    record: "20-3",
+    weight: "Bantamweight",
+    image: 'asterling.jpg'
+  },
+  {
+    id: 2,
+    name: "Petr11 Yan",
+    record: "16-2",
+    weight: "Bantamweight",
+    image: "pyan.jpg"
+  },
+  {
+    id: 3,
+    name: "TJ Dillashaw",
+    record: "17-4",
+    weight: "Bantamweight",
+    image: 'tdillashaw.jpg'
+  },
+  {
+    id: 4,
+    name: "Jose Aldo",
+    record: "31-7",
+    weight: "Bantamweight",
+    image: 'jaldo.jpg'
+  },
+  {
+    id: 5,
+    name: "Cory Sandhagen",
+    record: "14-4",
+    weight: "Bantamweight",
+    image: 'csandhagen.jpg'
+
+  },
+  {
+    id: 6,
+    name: "Alexander Volkanovski",
+    record: "23-1",
+    weight: "Featherweight",
+    image: 'avolkanovski.jpg'
+  },
+  {
+    id: 7,
+    name: "Max Holloway",
+    record: "23-6",
+    weight: "Featherweight",
+    image: 'mholloway.jpg'
+  },
+  {
+    id: 8,
+    name: "Brian Ortega",
+    record: "15-2-1",
+    weight: "Featherweight",
+    image: 'bortega.jpg'
+  },
+  {
+    id: 9,
+    name: "Yair Rodriguez",
+    record: "13-3-1",
+    weight: "Featherweight",
+    image: 'yrodriguez.jpg'
+
+  },
+  {
+    id: 10,
+    name: "Chan Sung Jung",
+    record: "17-6",
+    weight: "Featherweight",
+    image: 'cjung.jpg'
+  },
+  {
+    id: 11,
+    name: "Charles Oliveira",
+    record: "32-8-1",
+    weight: "Lightweight",
+    image: "coliveira.jpg"
+  },
+  {
+    id: 12,
+    name: "Justin Gaethje",
+    record: "23-3",
+    weight: "Lightweight",
+    image: "jgaethje.jpg"
+  },
+  {
+    id: 13,
+    name: "Dustin Poirier",
+    record: "28-7-1",
+    weight: "Lightweight",
+    image: "dpoirier.jpg"
+  },
+  {
+    id: 14,
+    name: "Islam Makhachev",
+    record: "22-1",
+    weight: "Lightweight",
+    image: "imakhachev.jpg"
+  },
+  {
+    id: 15,
+    name: "Beneil Dariush",
+    record: "21-4-1",
+    weight: "Lightweight",
+    image: "bdariush.jpg"
+  },
+  {
+    id: 16,
+    name: "Kamaru Usman",
+    record: "20-1",
+    weight: "Welterweight",
+    image: "kusman.jpg"
+  },
+  {
+    id: 17,
+    name: "Colby Covington",
+    record: "17-3",
+    weight: "Welterweight",
+    image: "ccovington.jpg"
+  },
+  {
+    id: 18,
+    name: "Gilbert Burns",
+    record: "20-4",
+    weight: "Welterweight",
+    image: "gburns.jpg"
+  },
+  {
+    id: 19,
+    name: "Leon Edwards",
+    record: "19-3-1",
+    weight: "Welterweight",
+    image: "ledwards.jpg"
+  },
+  {
+    id: 20,
+    name: "Vicente Luque",
+    record: "21-7-1",
+    weight: "Welterweight",
+    image: "vluque.jpg"
+  },
+]
+
+export default mock;
